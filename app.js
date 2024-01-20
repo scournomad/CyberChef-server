38c04fe895945d1487ea38a33960b06f62d163b1
Andrej Mohar <andrej.mohar@gmail.com>
2024-01-20 08:57:07 +0100

    Increased POST request size

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ if (process.env.NODE_ENV === "production") {
     }));
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: false }));
 app.use(cookieParser());
 
 
